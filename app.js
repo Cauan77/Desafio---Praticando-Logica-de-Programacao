5260c707ec442c1df7f1f4890fa0d9eb32c59829
Cauan Henrique <cauanhenrique56@gmail.com>
2025-02-23 19:49:03 -0300

    Finalização do projeto

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var nomes = [];
 let nome = "";
+let numeroAleatorioMaximo = 0;
+let numeroAleatorioMinimo = 0;
+var listaDeNumerosSorteados = [];
+let numeroAleatorio = 0;
+let n = 0;
 
 
 
@@ -8,7 +13,9 @@ function adicionarAmigo() {
     if (nome.trim() != "") {
         nomes.push(nome);
         console.log (nomes);
-        
+
+        lista();
+        numeroAleatorioMaximo++;
     } else {
         alert ('Nome inválido, tente inserir um nome novamente');
     }   
@@ -19,4 +26,33 @@ function limpaCampo () {
     document.getElementById('amigo').value = '';
 }
 
+function lista () {
+    const listaNomes = document.getElementById('listaAmigos');
+    listaNomes.innerHTML += '<li>' + nome + '</li>';
+    listaNomes.innerHTML += '';
+}
+
+function sortearAmigo () {
+    const resultadoNome = document.getElementById('resultado');
+    resultadoNome.innerHTML += '<li> O nome sorteado é: ' + nomes[gerarAmigoAleatorio()] + '</li>';
+
+}
+
+function gerarAmigoAleatorio () {
+    numeroAleatorioMinimo = Math.ceil(numeroAleatorioMinimo);
+    numeroAleatorioMaximo = Math.floor(numeroAleatorioMaximo);
+    numeroAleatorio = Math.floor(Math.random() * (numeroAleatorioMaximo - numeroAleatorioMinimo) + numeroAleatorioMinimo);
+
+    if (listaDeNumerosSorteados.includes(numeroAleatorio)) {
+        return gerarAmigoAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroAleatorio);
+        console.log(listaDeNumerosSorteados);
+        return numeroAleatorio;
+    }
+}
+
+
+
+console.log (numeroAleatorio);
 
